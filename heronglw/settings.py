b2e64084af9c8e76e5080f2dbe240e88c9e192b3
jansonzhou <546868234@qq.com>
2014-10-25 09:26:11 -0700

    add new css type

--- heronglw/settings.py
+++ heronglw/settings.py
@@ -84,7 +84,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, "static"),
-   os.path.join(BASE_DIR, "static/img")
+   os.path.join(BASE_DIR, "static/img"),
+   os.path.join(BASE_DIR, "static/js"),
+   os.path.join(BASE_DIR, "static/css"),
+
 )
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,'template'),
--- product/views.py
+++ product/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def index(request):
-    return render_to_response('base.html',{'title':('home')})
+    return render_to_response('index.html',{'title':('home')})
